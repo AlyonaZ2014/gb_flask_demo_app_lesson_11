69607d6baaf7c2fbcd78022d9d2006d8238ffe12
Sergey Ozeranskiy <sergey.ozeranskiy@gmail.com>
2021-07-21 20:10:23 +0300

    added API_URL config

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -6,6 +6,7 @@ from flask_login import login_required, current_user
 from sqlalchemy.orm import joinedload
 from werkzeug.exceptions import NotFound
 
+from blog.config import API_URL
 from blog.extensions import db
 from blog.forms.article import CreateArticleForm
 from blog.models import Article, Author, Tag
@@ -17,7 +18,7 @@ article = Blueprint('article', __name__, url_prefix='/articles', static_folder='
 def article_list():
     articles: Article = Article.query.all()
     # call RPC method
-    count_articles: Dict = requests.get('http://127.0.0.1:5000/api/articles/event_get_count/').json()
+    count_articles: Dict = requests.get(f'{API_URL}/api/articles/event_get_count/').json()
     return render_template(
         'articles/list.html',
         articles=articles,
